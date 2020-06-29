190b5bf0dbc13c1ca6d089804b924239a506ba43
Francisco Henriques <francisco.henriques@makewise.pt>
2020-06-29 10:13:49 +0100

    Fix json file saving

--- http_dummy.py
+++ http_dummy.py
@@ -1,35 +1,32 @@
 #CODIGO INCOMPLETO NA LINHA 73
 
 from json.decoder import JSONDecodeError
-
+from logging import DEBUG
+import logging
 from flask import Flask, request, json  # import main Flask class and request object
 
 http_dummy_server = Flask(__name__, static_url_path="", static_folder="./webapp", )  # create the Flask app
+logging.basicConfig()
+LOG_APP = logging.getLogger('app')
 
 # Global variables to store info
 
-#LABEL = None
-#TOTAL_EXIT = 0
-#TOTAL_ENTER = 0
-#MAX_PEOPLE = 0
+LABEL = None
+TOTAL_EXIT = 0
+TOTAL_ENTER = 0
+MAX_PEOPLE = 0
 
 valueList = []
-valueData = {"label": None, "enter": None, "exit": None, "maxx": None}
 
 try:
     with open('output.json', 'r') as JSON:
         valueList = json.load(JSON)
-        #print(valueList)
-except: # essa parte não é necessária conforme o código atual
+except Exception as ex:
+    LOG_APP.info('output.json not found')
     print("no output.json found")
-#    with open('output.json', 'w') as JSON:
- #       TOTAL_EXIT = 0
-  #      TOTAL_ENTER = 0
-   #     MAX_PEOPLE = 0
-    #    json.dump({"enter": 0, "exit": 0, "maxx": 25}, JSON)
 
-for item in valueList: #idog iperson
-    print("a "+item["label"])
+LOG_APP.info('History: %s' % str(valueList))
+
 
 def what_color():
     if (TOTAL_ENTER - TOTAL_EXIT) >= MAX_PEOPLE:
@@ -71,22 +68,33 @@ def counting_json(sensor_id):
 @http_dummy_server.route('/form/<sensor_id>', methods=['POST'])
 def counting_post(sensor_id):
     global valueList
-
-    req_data = request.values #INCOMPLETO!!!!!
-
-    print("testetesteteste" + req_data["label"])
-
-    if req_data["label"] in valueList:
-        for item in valueList:  # idog iperson
-            if req_data["label"] == item["label"]:
-                item["enter"] += int(req_data["enter"])
-                print(item["enter"])
-                item["exit"] += int(req_data["exit"])
-    else:
+    LOG_APP.info('New counting recevied for device %s' % str(sensor_id))
+    req_data = request.values
+    found = False
+    for item in valueList:
+        if req_data["label"] == item["label"]:
+            found = True
+            LOG_APP.info('Label %s already exists! updating values...' % str(req_data["label"]))
+            item["enter"] += int(req_data["enter"])
+            item["exit"] += int(req_data["exit"])
+    if not found:
+        LOG_APP.info('Label %s does not exist! appending new value...' % str(req_data["label"]))
         valueList.append(req_data)
+    save_data()
+    response = http_dummy_server.response_class(
+        response=json.dumps(valueList),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
-    print("Revice counting from sensor %s = %s" % (sensor_id, (req_data['enter'], req_data['exit'])))
-    return countings()
+
+def save_data():
+    try:
+        with open('output.json', 'w') as JSON:  # PLANO > append da informação
+            json.dump(valueList, JSON)
+    except Exception as ex:
+        LOG_APP.exception("Error saving history file", ex)
 
 
 # Return current counting
